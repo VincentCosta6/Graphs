ae9affde6324d4d0c449f44b553b250167d15ef8
Vincent Costa <costa.vincent132@gmail.com>
2020-01-22 14:56:57 -0800

    Ancestor is now finally working

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -23,7 +23,6 @@ def earliest_ancestor(ancestors, starting_node):
         d = path[-1]
 
         if (len(path) >= max_path and d < earliest) or (len(path) > max_path):
-            print(d)
             max_path = len(path)
             earliest = d
         
@@ -31,10 +30,8 @@ def earliest_ancestor(ancestors, starting_node):
             copy = path[:]
             copy.append(neighbor)
 
-            print("do it")
             queue.enqueue(copy)
 
-    print(f"return {earliest}")
     return earliest
 
 test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
