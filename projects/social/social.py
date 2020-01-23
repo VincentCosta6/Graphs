89ea0f113fb76314cf42c4c197a7d3e5be19389e
Vincent Costa <costa.vincent132@gmail.com>
2020-01-22 16:25:47 -0800

    random friendships

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,6 +1,11 @@
+import random
+import re
+
 class User:
     def __init__(self, name):
         self.name = name
+    def __str__(self):
+        return f"{self.name}"
 
 class SocialGraph:
     def __init__(self):
@@ -14,11 +19,14 @@ class SocialGraph:
         """
         if user_id == friend_id:
             print("WARNING: You cannot be friends with yourself")
+            return False
         elif friend_id in self.friendships[user_id] or user_id in self.friendships[friend_id]:
             print("WARNING: Friendship already exists")
+            return False
         else:
             self.friendships[user_id].add(friend_id)
             self.friendships[friend_id].add(user_id)
+            return True
 
     def add_user(self, name):
         """
@@ -27,6 +35,8 @@ class SocialGraph:
         self.last_id += 1  # automatically increment the ID to assign the new user
         self.users[self.last_id] = User(name)
         self.friendships[self.last_id] = set()
+
+        return self.last_id
 
     def populate_graph(self, num_users, avg_friendships):
         """
@@ -44,9 +54,46 @@ class SocialGraph:
         self.friendships = {}
         # !!!! IMPLEMENT ME
 
+        user_ids = []
+
+        alpha = list("abcdefghijklmnopqrstuvwxyz")
+
+        random.shuffle(alpha)
+
         # Add users
+        for i in range(num_users):
+            name_range = random.randrange(3, 15)
+
+            user_id = self.add_user("".join(alpha[:name_range]))
+
+            random.shuffle(alpha)
+            user_ids.append(user_id)
 
         # Create friendships
+        max_friendships = avg_friendships * 2
+
+        for i in range(num_users):
+            friend_count = random.randrange(1, max_friendships)
+            friendships_created = 0
+
+            loops = 0
+
+            while friendships_created <= friend_count:
+                friend_id = random.randrange(1, num_users)
+
+                temp_i = i + 1
+
+                first =temp_i if temp_i < friend_id else friend_id
+                second = friend_id if friend_id > temp_i else temp_i 
+
+                if self.add_friendship(first, second):
+                    friendships_created += 1
+
+                loops += 1
+
+                if loops > 10000:
+                    break
+
 
     def get_all_social_paths(self, user_id):
         """
@@ -65,6 +112,7 @@ class SocialGraph:
 if __name__ == '__main__':
     sg = SocialGraph()
     sg.populate_graph(10, 2)
+    print(sg.users)
     print(sg.friendships)
     connections = sg.get_all_social_paths(1)
     print(connections)
